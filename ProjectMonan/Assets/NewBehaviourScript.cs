19a43bc12f39765dedbb775a5e461972d2123948
Bartô Alheiros <alheirosb@gmail.com>
2020-10-04 20:38:12 -0300

    Sprites Monan
    Adicionados Sprites Andar e Pular.
    Adicionado Tile do Chão e Background Floresta Amazônica.

---
+++ ProjectMonan/Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPreview(typeof(GameObject))]
+public class MyPreview : ObjectPreview
+{
+    public override bool HasPreviewGUI()
+    {
+        return true;
+    }
+
+    public override void OnPreviewGUI(Rect r, GUIStyle background)
+    {
+        GUI.Label(r, target.name + " is being previewed");
+    }
+}
